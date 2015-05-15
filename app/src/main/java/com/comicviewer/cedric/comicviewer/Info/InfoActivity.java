107d4b703c3600c4d2f11176a7b4b36da7fd881f
Cédric Vandelaer <cedric.vandelaer@gmail.com>
2015-05-15 21:32:36 +0200

    File info bugfixes

--- app/src/main/java/com/comicviewer/cedric/comicviewer/Info/InfoActivity.java
+++ app/src/main/java/com/comicviewer/cedric/comicviewer/Info/InfoActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,11 +15,17 @@ import com.comicviewer.cedric.comicviewer.R;
 import com.comicviewer.cedric.comicviewer.Utilities;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
+import java.io.File;
+
 public class InfoActivity extends Activity {
 
     private Comic mComic = null;
     private TextView mTitleTextView;
     private ImageView mCoverImageView;
+    private TextView mIssueNumberTextView;
+    private TextView mYearTextView;
+    private TextView mFilenameTextView;
+    private TextView mFileSizeTextView;
 
 
     @Override
@@ -42,12 +49,40 @@ public class InfoActivity extends Activity {
 
         mTitleTextView = (TextView) findViewById(R.id.title_text_view);
         mCoverImageView = (ImageView) findViewById(R.id.cover_image_view);
+        mIssueNumberTextView = (TextView) findViewById(R.id.issue_number_text_view);
+        mYearTextView = (TextView) findViewById(R.id.year_text_view);
+        mFilenameTextView = (TextView) findViewById(R.id.filename_text_view);
+        mFileSizeTextView = (TextView) findViewById(R.id.filesize_text_view);
 
         mTitleTextView.setText(mComic.getTitle());
         mTitleTextView.setBackgroundColor(mComic.getComicColor());
         mTitleTextView.setTextColor(mComic.getPrimaryTextColor());
 
-        ImageLoader.getInstance().displayImage(mComic.getCoverImage(),mCoverImageView);
+        if (mComic.getCoverImage()!=null)
+            ImageLoader.getInstance().displayImage(mComic.getCoverImage(),mCoverImageView);
+
+        if (mComic.getIssueNumber()!=-1)
+        {
+            mIssueNumberTextView.setText("Issue number: "+mComic.getIssueNumber());
+        }
+        else
+        {
+            mIssueNumberTextView.setVisibility(View.GONE);
+        }
+
+        if (mComic.getYear()!=-1)
+        {
+            mYearTextView.setText("Year: "+mComic.getYear());
+        }
+        else
+        {
+            mYearTextView.setVisibility(View.GONE);
+        }
+
+        File archiveFile = new File(mComic.getFilePath()+"/"+mComic.getFileName());
+
+        mFilenameTextView.setText("Filename:\n"+archiveFile.getName());
+        mFileSizeTextView.setText("File size: "+archiveFile.length()/(1024*1024)+" mb");
 
     }
 
